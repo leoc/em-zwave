312e03a66dcea765aaab1f5dad6e212f1ae7a791
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-10-06 14:34:03 +0200

    Move Zwave class into extra file

--- lib/em-zwave.rb
+++ lib/em-zwave.rb
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 require 'eventmachine'
+require 'em-zwave/zwave'
+require 'em-zwave/controller'
+require 'em-zwave/value'
+require 'em-zwave/node'
 require 'em-zwave/notification'
 require 'emzwave'
 
@@ -7,79 +11,6 @@ require 'fileutils'
 
 module EventMachine
   class Zwave
-    attr_reader :devices, :config_path
-
-    def initialize(options = {})
-      options[:devices] = [options.delete(:device)] if options.key?(:device)
-      options = {
-        devices: [],
-        config_path: '.'
-      }.merge(options)
-
-      @devices = options[:devices].uniq
-      @config_path = File.expand_path(options[:config_path])
-      @stop_emzwave = false
-      @scheduled_notification_count = 0
-      @overall_notifications = 0
-
-      @queue = EM::Queue.new
-      @queue.pop(&method(:notification_received))
-
-      # the initialization should be run in the next tick, so that the
-      # method #add_device may be called within the same block in
-      # which the EM::Zwave class was instantiated.
-      EM.next_tick do
-        initialize_zwave
-      end
-    end
-
-    def add_device(device)
-      @devices << device unless @devices.include?(device)
-    end
-
-    def push_notification(notification)
-      @scheduled_notification_count += 1
-      @queue.push(notification)
-    end
-
-    def notification_received(notification)
-      @overall_notifications += 1
-      callbacks.each do |cb|
-        cb.call(notification)
-      end
-      @scheduled_notification_count -= 1
-      if @stop_emzwave && @scheduled_notification_count <= 0
-        invoke_shutdown_callbacks
-      else
-        @queue.pop(&method(:notification_received))
-      end
-    end
-
-    def invoke_shutdown_callbacks
-      shutdown_callbacks.each(&:call)
-    end
-
-    def schedule_shutdown
-      EM.schedule do
-        @stop_emzwave = true
-        invoke_shutdown_callbacks if @scheduled_notification_count <= 0
-      end
-    end
-
-    def on_notification(&block)
-      callbacks << block
-    end
-
-    def on_shutdown(&block)
-      shutdown_callbacks << block
-    end
-
-    def shutdown_callbacks
-      @shutdown_callbacks ||= []
-    end
-
-    def callbacks
-      @callbacks ||= []
-    end
+    VERSION = '0.0.1'
   end
 end
