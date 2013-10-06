00f7dc15cc13f583f9a814f1f9e232d718c2eddd
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-10-06 17:12:55 +0200

    Add Node#security_device?

--- ext/em_zwave.cpp
+++ ext/em_zwave.cpp
@@ -544,6 +544,15 @@ VALUE rb_node_is_routing_device(VALUE self)
 }
 
 extern "C"
+VALUE rb_node_is_security_device(VALUE self)
+{
+    uint32 home_id = FIX2UINT(rb_iv_get(self, "@home_id"));
+    uint8 node_id = FIX2UINT(rb_iv_get(self, "@node_id"));
+    bool is_security_device = Manager::Get()->IsNodeSecurityDevice(home_id, node_id);
+    return (is_security_device ? Qtrue : Qfalse);
+}
+
+extern "C"
 VALUE rb_node_on(VALUE self)
 {
     uint32 home_id = FIX2UINT(rb_iv_get(self, "@home_id"));
@@ -942,6 +951,7 @@ void Init_emzwave() {
     rb_define_method(rb_cNode, "frequent_listening_device?", (VALUE (*)(...))rb_node_is_frequent_listening_device, 0);
     rb_define_method(rb_cNode, "beaming_device?", (VALUE (*)(...))rb_node_is_beaming_device, 0);
     rb_define_method(rb_cNode, "routing_device?", (VALUE (*)(...))rb_node_is_routing_device, 0);
+    rb_define_method(rb_cNode, "security_device?", (VALUE (*)(...))rb_node_is_security_device, 0);
     //rb_define_method(rb_cZwave, "values", (VALUE (*)(...))rb_node_values, 0);
 
     rb_cValue = rb_define_class_under(rb_cZwave, "Value", rb_cObject);
