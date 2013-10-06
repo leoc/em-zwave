079434dca19a7dd2d58bfec9c8bf85fdc97dbb19
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-10-06 16:12:17 +0200

    Remove controller requirement

--- lib/em-zwave.rb
+++ lib/em-zwave.rb
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 require 'eventmachine'
 require 'em-zwave/zwave'
-require 'em-zwave/controller'
 require 'em-zwave/value'
 require 'em-zwave/node'
 require 'em-zwave/notification'
