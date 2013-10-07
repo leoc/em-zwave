ab57319efaaf075d58cef011bb25b821556fafab
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-10-07 23:46:44 +0200

    Add some real examples to usage.rb

--- usage.rb
+++ usage.rb
@@ -11,19 +11,30 @@ EM.run do
     puts "Notification in user code: #{notification}"
   end
 
-  @zwave.on(:value_changed) do |value|
-    if value.label == "Level" && value.get > 50
-      EM::Timer.new(5) do
-        value.set(10)
+  @zwave.on_initialization_finished do
+    puts "Initialization has been finished"
+
+    @zwave.nodes.each_value do |node|
+      puts node.inspect
+      node.values.each do |value|
+        puts " - #{value.inspect}"
       end
     end
+
+    @zwave.nodes.each_value do |node|
+      node.level = 99
+    end
+  end
+
+  @zwave.on(:value_changed) do |value|
+    puts value.inspect
   end
 
   @zwave.on_shutdown do
     EM.stop
   end
 
-  Signal.trap('INT')  { @zwave.shutdown }
+  Signal.trap('INT') { @zwave.shutdown }
   Signal.trap('TERM') { @zwave.shutdown }
 
   EM::PeriodicTimer.new(1) { puts 'R: .' }
